4c66d7f9c32065754c883270526cdb93a335ab7e
wslyvh <wslyvh@users.noreply.github.com>
2025-05-15 22:39:38 +0200

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,46 +13,44 @@ import { Layout } from "@/components/layout";
 import { Providers } from "@/context";
 import "@/assets/globals.css";
 
-export async function generateMetadata(): Promise<Metadata> {
-  return {
-    applicationName: APP_NAME,
-    title: {
-      default: `${APP_NAME} · ${APP_DESCRIPTION}`,
-      template: `%s · ${APP_NAME}`,
-    },
-    metadataBase: new URL(APP_URL),
+export const metadata: Metadata = {
+  applicationName: APP_NAME,
+  title: {
+    default: `${APP_NAME} · ${APP_DESCRIPTION}`,
+    template: `%s · ${APP_NAME}`,
+  },
+  metadataBase: new URL(APP_URL),
+  description: APP_DESCRIPTION,
+  openGraph: {
+    type: "website",
+    title: APP_NAME,
+    siteName: APP_NAME,
     description: APP_DESCRIPTION,
-    openGraph: {
-      type: "website",
-      title: APP_NAME,
-      siteName: APP_NAME,
-      description: APP_DESCRIPTION,
-      url: APP_URL,
-      images: APP_OG_IMAGE,
-    },
-    twitter: {
-      card: "summary_large_image",
-      site: SOCIAL_TWITTER,
-      title: APP_NAME,
-      description: APP_DESCRIPTION,
-      images: APP_OG_IMAGE,
-    },
-    other: {
-      "fc:frame": JSON.stringify({
-        version: APP_FRAME_VERSION,
-        imageUrl: APP_IMAGE,
-        button: {
-          title: APP_NAME,
-          action: {
-            type: "launch_frame",
-            name: APP_NAME,
-            url: APP_URL,
-          },
+    url: APP_URL,
+    images: APP_OG_IMAGE,
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: SOCIAL_TWITTER,
+    title: APP_NAME,
+    description: APP_DESCRIPTION,
+    images: APP_OG_IMAGE,
+  },
+  other: {
+    "fc:frame": JSON.stringify({
+      version: APP_FRAME_VERSION,
+      imageUrl: APP_IMAGE,
+      button: {
+        title: APP_NAME,
+        action: {
+          type: "launch_frame",
+          name: APP_NAME,
+          url: APP_URL,
         },
-      }),
-    },
-  };
-}
+      },
+    }),
+  },
+};
 
 export const viewport: Viewport = {
   width: "device-width",
